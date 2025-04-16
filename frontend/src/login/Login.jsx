31e8360a84db71635ddd4614515bf00ef5d2fee8
Rupesh kumar <rupesh18123143@gmail.com>
2025-04-17 00:51:01 +0530

    add login to backend

--- frontend/src/login/Login.jsx
+++ frontend/src/login/Login.jsx
@@ -1,21 +1,61 @@
 
 
 
-
+import { useRef } from "react";
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 import './login.css'; // Assuming you have CSS file for styling
-import { Link } from 'react-router-dom';
-import videoSrc from './12345.mp4';
+import { Link,useNavigate } from 'react-router-dom';
+
 function Login() {
+
+ 
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+   
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    if (email || password) {
+      let result = await fetch("http://localhost:3000/api/login", {
+        method: "post",
+        body: JSON.stringify({ email, password }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      alert("login sucessfully"+{result});
+      navigate("/");
+      console.log(result);
+      const data = await result.json();
+      console.log(data);
+      if (data.msg === "successful_login") {
+        const loginButton = document.querySelector("#LoginButton");
+        if (loginButton) {
+          loginButton.textContent = data.data || "user"; // Assuming the response contains the user's name
+          loginButton.style.border = "none";
+          loginButton.style.background = "none";
+          loginButton.style.cursor = "default";
+          loginButton.removeEventListener("click", handleSubmit);
+        }
+      }
+
+    } else {
+      alert("plese fill all the input box");
+    }
+  };
+
+
+
   return (
     <div className="login-container">
-{/* video-background */}
-{/* <video autoPlay muted loop className=" vid">
-        <source src={videoSrc} type="video/mp4" />
-        Add additional source elements for different video formats if needed
-      </video> */}
+
       <div className="vid">
           <lottie-player
             src="https://lottie.host/cd6f307b-3465-4241-98a2-5cf1b06e4d12/0On4nB4CeC.json"
@@ -32,14 +72,21 @@ function Login() {
         <h2>Login</h2>
         <div className="input-container">
           <FontAwesomeIcon icon={faUser} className="icon" />
-          <input type="text" placeholder="Username" />
+          <input  type="text"
+              placeholder="Your email"
+              ref={emailRef}
+              required />
         </div>
         <div className="input-container">
           <FontAwesomeIcon icon={faLock} className="icon" />
-          <input type="password" placeholder="Password" />
+          <input type="password"
+              placeholder="Your Password"
+              ref={passwordRef}
+              required />
         </div>
         <div className='l1'>
-        <button className="login-button">Login</button>
+        
+        <button className="login-button" onClick={handleSubmit}>Login</button>
         <Link to={'/signup'} className="login-button t10">
           sign up
         </Link>
